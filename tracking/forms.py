6da10b0eacfcccf648651ec1e0f2d0a9907d561a
Justin Zabilansky <jzab@mit.edu>
2015-07-21 15:50:20 -0400

    Edit Flat Capability
    Edit Flat Button is now functional but instead of editing a given flat
    and updating it in the db it adds a new flat.

--- tracking/forms.py
+++ tracking/forms.py
@@ -6,5 +6,5 @@ from .models import Position
 class FlatForm(ModelForm):
 	class Meta:
 		model = Position
-		fields = ['position', 'tray_tag', 'kind', 'variety', 'seed_volume', 'supplier',
+		fields = ['tray_tag', 'kind', 'variety', 'seed_volume', 'supplier',
 				'date_seeded', 'date_uncovered', 'date_harvested']
--- tracking/views.py
+++ tracking/views.py
@@ -5,6 +5,7 @@ from django.forms import ModelForm
 from django.utils import timezone
 
 from .models import Farm, Unit, Level, Position, FlatForm
+from .forms import FlatForm
 
 
 
@@ -27,7 +28,7 @@ def levels(request, farm_address, unit_id):
 
 def positions(request, farm_address, unit_id, level_id):
 	if request.method == 'POST':
-		pos = request.POST['position']
+		tag = request.POST['tray_tag']
 		kind = request.POST['kind']
 		var = request.POST['variety']
 		vol = request.POST['seed_volume']
@@ -35,8 +36,8 @@ def positions(request, farm_address, unit_id, level_id):
 		ds = request.POST['date_seeded']
 		du = request.POST['date_uncovered']
 		dh = request.POST['date_harvested']
-		new_pos = Position(id=1,
-							tray_tag=pos,
+		current_level = Farm.objects.get(pk=farm_address).unit_set.get(pk=unit_id).level_set.get(pk=level_id)
+		new_pos = Position(tray_tag=tag,
 							kind=kind,
 							variety=var,
 							seed_volume=vol,
@@ -47,7 +48,7 @@ def positions(request, farm_address, unit_id, level_id):
 							level=Farm.objects.get(pk=farm_address).unit_set.get(pk=unit_id).level_set.get(pk=level_id))
 		new_pos.save()
 		position_data = Farm.objects.get(pk=farm_address).unit_set.get(pk=unit_id).level_set.get(pk=level_id).position_set.all()
-		context = {'position_data': position_data}
+		context = {'position_data': position_data, 'tag':tag,'kind':kind,'var':var,'vol':vol,'sup':sup,'ds':ds,'du':du,'dh':dh,'current_level':current_level}
 		return render(request, 'tracking/position.html',context)
 	else:
 		position_data = Farm.objects.get(pk=farm_address).unit_set.get(pk=unit_id).level_set.get(pk=level_id).position_set.all()
@@ -55,8 +56,33 @@ def positions(request, farm_address, unit_id, level_id):
 		return render(request, 'tracking/position.html', context)
 
 def edit_flat(request, farm_address, unit_id, level_id, position_id):
+	pos = Farm.objects.get(pk=farm_address).unit_set.get(pk=unit_id).level_set.get(pk=level_id).position_set.get(pk=position_id)
+	form = FlatForm(instance=pos)
+	context = {'form':form,
+				'level':pos.level,
+				'pos':pos,
+				'tray_tag':pos.tray_tag,
+				'kind':pos.kind,
+				'variety':pos.variety,
+				'seed_volume':pos.seed_volume,
+				'supplier':pos.supplier,
+				'date_seeded':pos.date_seeded,
+				'date_uncovered':pos.date_uncovered,
+				'date_harvested':pos.date_harvested}
+	variables = RequestContext(request, context)
 	if request.method == 'POST':
-		return render(request, 'tracking/edit_flat.html')
+		form = FlatForm(request.POST, instance=pos)
+		if form.is_valid():
+			pos.tray_tag = form.cleaned_data['tray_tag']
+			pos.kind = form.cleaned_data['kind']
+			pos.variety = form.cleaned_data['variety']
+			pos.seed_volume = form.cleaned_data['seed_volume']
+			pos.supplier = form.cleaned_data['supplier']
+			pos.date_seeded = form.cleaned_data['date_seeded']
+			pos.date_uncovered = form.cleaned_data['date_uncovered']
+			pos.date_harvested = form.cleaned_data['date_harvested']
+			return render_to_response('tracking/position.html', variables)
+	return render(request,'tracking/edit_flat.html',context)
 
 def harvest_flat(request, farm_address, unit_id, level_id, position_id):
 	# if request.method == 'POST':
